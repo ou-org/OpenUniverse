9ad534d6c8019511161c6b378ae3b969b27dd157
k2Xzny <no-reply@example.com>
2025-09-20 22:51:25 +0300

    cm

--- src/main/java/org/ou/common/utils/TerminalUtils.java
+++ src/main/java/org/ou/common/utils/TerminalUtils.java
@@ -1,7 +1,9 @@
 package org.ou.common.utils;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
@@ -30,7 +32,11 @@ public class TerminalUtils {
             prompt += ' ';
         }
         System.err.print(prompt);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+        InputStream in = System.in;
+        if (System.console() == null) {
+            in = new FileInputStream("/dev/tty");
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
         String answer = reader.readLine();
         if (answer == null) {
             return false;
